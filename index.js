9c46e0a53b71400237d8a6784a59f8eb2aa2303d
Claudiney Nascimento <contato@claudiney.info>
2022-11-04 04:11:07 -0300

    Update files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(new Date().toLocaleString());
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || '8080';
+
+app.get('/', (req, res) => {
+  return res.send('Hello from Openshift Local');
+});
+
+app.listen(parseInt(port, 10), ()=>console.log(`Application started on port ${port}`));
+
